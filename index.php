c732a13bb0d577f34f2c6b7ad05623f826658417
schneiderjd <jozschsms@gmail.com>
2024-07-28 22:36:22 -0500

    Added project files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="main.css">
+        <script src="main.js"></script>
+        <title>Jojobaaaaa</title>
+    </head>
+    <body>
+        <header></header>
+        <p>hello world</p>
+        <footer></footer>
+</body>
+</html>
